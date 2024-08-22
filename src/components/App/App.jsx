6ce8a88456209431891dace6cbff9f4bc9e7b246
Emma Molden <emma.molden007@gmail.com>
2024-08-22 16:30:38 -0500

    App has Nav bar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,27 @@
+import FavoritePage from '../FavoritePage/FavoritePage.jsx'
+import SearchPage from '../SearchPage/SearchPage.jsx'
+import {
+  HashRouter as Router,
+  Route,
+  Link,
+  useLocation
+  } from "react-router-dom/cjs/react-router-dom.min.js";
+  import React from 'react';
+
 function App() {
   return (
     <div>
       <h1>Giphy Search!</h1>
+
+      <Router>    
+        <Route exact path="/">
+          <SearchPage />
+        </Route>
+
+        <Route exact path="/favorites">
+          <FavoritePage />
+        </Route>
+      </Router>
     </div>
   );
 }
